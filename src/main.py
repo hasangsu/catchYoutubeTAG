148a03dd2b6466b1120413ae351d6f8c91f95d60
hasangsu <ssha@autocrypt.io>
2024-06-08 18:53:59 +0900

    append function catch youtube title, tag, hash_tag

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import sys
 import re
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
 # check url is youtube?
@@ -12,40 +15,105 @@ def is_youtube_link(url):
 
     return re.match(youtube_regex, url) is not None
 
-# check sys argv count
-if len(sys.argv) != 2 :
-    print("the argv count is invalid.")
-    sys.exit()
+def check_vaild_youtube():
+    # check sys argv count
+    if len(sys.argv) != 2 :
+        print("the argv count is invalid.")
+        return False
 
-# check youtube url
-url = sys.argv[1]
-result = is_youtube_link(url)
-if not result :
-    print("url is not youtube url.")
-    sys.exit()
+    # check youtube url
+    url = sys.argv[1]
+    result = is_youtube_link(url)
+    if not result :
+        print("url is not youtube url.")
+        return False
+
+    return True
+
+def catch_title(soup):
+    title = (soup.find("div", attrs={"id": "title", "class": "ytd-watch-metadata"})).find("yt-formatted-string").text
+    return title
+
+def catch_tag(soup):
+    meta_tags = soup.find_all('meta', property="og:video:tag")
+    contnt_tags = [tag['content'] for tag in meta_tags]
+    tag = ', '.join(contnt_tags)
+    return tag
+
+def catch_hash_tag(soup):
+    expender_contents = soup.find_all("a", class_="yt-core-attributed-string__link--display-type")
+
+    contnt_tags = []
+    for content in expender_contents:
+        if content.text[0] == "#":
+            contnt_tags.append(content.text)
     
-# .get() is sends a GET request
-# return is server’s response to an HTTP request
-# if the response code is 200 OK
-response = requests.get(url)
-if not response.ok:
-    print("fail request.")
-    sys.exit()
+    hash_tag = ' '.join(contnt_tags)
+    return hash_tag
 
-# get url html
-soup = BeautifulSoup(response.text, 'html.parser')
+def run_selenium(target_url):
+    # create selenium options
+    options = webdriver.ChromeOptions()
 
-# get movie title in url html
-# title = soup.select_one('meta[property="og:title"]')['content']
-title = soup.select_one('meta', property='og:title')['content']
+    # hide selenium dynamic web page
+    options.add_argument("headless")
 
-# get movie hash tag in url html (TBD)
+    # run selenium
+    driver = webdriver.Chrome(options=options)
+    driver.get(target_url)
+    
+    # wait web dynamic loading
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "title"))
+    )
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "description-text-container"))
+    )
+    
+    return driver
 
-# get movie tag in url html
-meta_tags = soup.find_all('meta', property="og:video:tag")
-contnt_tags = [tag['content'] for tag in meta_tags]
-tags = ', '.join(contnt_tags)
+def stop_selenium(driver):
+    driver.quit()
+    return
 
-# print parse result
-print(title)
-print(tags)
+def output_catch_information(title, tags, hash_tag):
+    green_color = '\033[32m'
+    reset_color = '\033[0m'
+    
+    print(f"{green_color}Catch Information (Title, Tag, Hash Tag){reset_color}")
+    print(title)
+    print(tags)
+    print(hash_tag)
+    return
+
+# start main catch youtube
+def start_catch_youtube():
+    is_valid = check_vaild_youtube()
+    if not is_valid :
+        sys.exit()
+        return
+    
+    target_url = sys.argv[1]
+
+    # run selenium dynamic web page
+    driver = run_selenium(target_url)
+
+    # get url html
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    # get movie title in url html
+    title = catch_title(soup)
+
+    # get movie tag in url html
+    tag = catch_tag(soup)
+
+    # get movie hash tag in url html
+    hash_tag = catch_hash_tag(soup)
+
+    # output catch information
+    output_catch_information(title, tag, hash_tag)
+
+    stop_selenium(driver)
+    return
+
+start_catch_youtube()
